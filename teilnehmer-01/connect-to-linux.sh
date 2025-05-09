8cc090f660a05c856c3b8b23000e0dd2e17a00cc
Frank Pommerening <frank@pommerening-online.de>
2025-05-09 22:03:54 +0200

    init

--- teilnehmer-01/connect-to-linux.sh
+++ teilnehmer-01/connect-to-linux.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 ssh -i /share/.ssh/rsa_key teilnehmer@linux-teilnehmer-01.workshop echo "Connection checked"
-cp /share/data/docker-aachen/ssh/teilnehmer-01/config /home/coder/.ssh/config
+cp /share/data/connect-to-dockerteilnehmer-01/config /home/coder/.ssh/config
 export DOCKER_HOST=ssh://teilnehmer@linux-teilnehmer-01.workshop
 echo "Connection to Linux done"
--- trainer/connect-to-linux.sh
+++ trainer/connect-to-linux.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 ssh -i /share/.ssh/rsa_key teilnehmer@linux-trainer.workshop echo "Connection checked"
-cp /share/data/docker-aachen/ssh/trainer/config /home/coder/.ssh/config
+cp /share/data/connect-to-dockertrainer/config /home/coder/.ssh/config
 export DOCKER_HOST=ssh://teilnehmer@linux-trainer.workshop
 echo "Connection to Linux done"
